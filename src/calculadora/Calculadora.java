4be45c81c9dd81f7492dff53fe1ff134205228d0
Mauro <Mauro@Mauro>
2012-08-02 18:48:15 -0300

    modifique

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+/**
+ *
+ * @author Mauro Federico Lopez
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CalculadoraUI calculadora = new CalculadoraUI();
+        calculadora.setVisible(true);
+    }
+}
